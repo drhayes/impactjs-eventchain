8b37451b6a4a956cb3eb724089053be81bb85623
David Hayes <drhayes@gmail.com>
2013-01-20 21:53:54 -0800

    Test all the things.

--- test/eventChainSpec.js
+++ test/eventChainSpec.js
@@ -1,7 +1,9 @@
+/*global require: true, global: true, describe: true, beforeEach: true,
+  it: true, EventChain: true */
 var assert = require('assert');
 
 // Fake the Impact global namespace with good enough definitions.
-global.ig = {
+var ig = global.ig = {
   // Properties used by eventChain.
   system: {
     tick: 0
@@ -36,6 +38,10 @@ describe('eventChain', function() {
     operators.forEach(function(operator) {
       assert(chain[operator]);
     });
+  });
+
+  it('is itself a function', function() {
+    assert(typeof chain === 'function');
   });
 
   it('has a then function that executes every invocation', function() {
@@ -73,5 +79,66 @@ describe('eventChain', function() {
     chain();
     chain();
     assert(done);
+  });
+
+  it('can do stuff during a wait', function() {
+    var duringCounter = 0;
+    var done = false;
+    chain
+      .wait(2)
+      .during(function() {
+        duringCounter += 1;
+      })
+      .then(function() {
+        done = true;
+      });
+    ig.system.tick = 1;
+    chain();
+    chain();
+    chain();
+    assert(duringCounter === 2);
+    assert(done);
+  });
+
+  it('can repeat steps', function() {
+    var repeat1 = 0;
+    var repeat2 = 0;
+    chain
+      .then(function() {
+        repeat1 += 1;
+      })
+      .repeat(2)
+      .then(function() {
+        repeat2 += 1;
+      })
+      .repeat(2);
+    chain(); // First then
+    chain(); // First repeat
+    chain(); // First then again
+    chain(); // First repeat again
+    chain(); // Second then
+    chain(); // Second repeat
+    chain(); // First then 3
+    chain(); // First repeat 3
+    chain(); // First then 4
+    chain(); // First repeat 4
+    chain(); // Second then 2
+    assert(repeat1 === 4);
+    assert(repeat2 === 2);
+  });
+
+  it('can do something every N seconds', function() {
+    ig.system.tick = .1;
+    var counter = 0;
+    chain
+      .wait(5)
+      .every(1, function() {
+        counter += 1;
+      });
+    // Turns out that "wait" isn't an exact waiter.
+    for (var i = 0; i < 75; i++) {
+      chain();
+    }
+    assert(counter === 4);
   });
 });
